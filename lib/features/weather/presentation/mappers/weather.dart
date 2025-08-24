d11729e5c424b11d281cd540b41178bbeb004ee9
Urban Potato <veryprogramming@gmail.com>
2025-08-24 16:25:05 +0700

    Add Domain Model to UI Model mapper

---
+++ lib/features/weather/presentation/mappers/weather.dart
@@ -0,0 +1,171 @@
+import '../../domain/models/index.dart';
+import '../../shared/utils/moon_phase_image_path_helper/index.dart';
+import '../models/index.dart';
+
+extension on WeatherModelDomain {
+  WeatherModelUI toWeatherModelUI() {
+    final todayDomain = forecast.forecastDayList[0];
+
+    final todayMainWeatherUI = MainWeatherModelUI(
+      temperature: TemperatureModelUI(
+        celsius: current.temperature.celsius.round(),
+        fahrenheit: current.temperature.fahrenheit.round(),
+      ),
+      condition: ConditionModelUI(
+        text: current.condition.text,
+        iconPath: current.condition.iconPath,
+      ),
+      temperatureRange: TemperatureRangeModelUI(
+        maximum: TemperatureModelUI(
+          celsius: todayDomain.day.maxTemp.celsius.round(),
+          fahrenheit: todayDomain.day.maxTemp.fahrenheit.round(),
+        ),
+        minimum: TemperatureModelUI(
+          celsius: todayDomain.day.minTemp.celsius.round(),
+          fahrenheit: todayDomain.day.minTemp.fahrenheit.round(),
+        ),
+      ),
+      uv: current.uv.round(),
+      airQualityGbDefraIndex: current.airQuality.gbDefraIndex,
+    );
+
+    final weeklyForecastPreviewUI = forecast.forecastDayList.take(3).map((f) {
+      return WeeklyForecastPreviewDayModelUI(
+        dateTime: f.date,
+        condition: ConditionModelUI(
+          text: f.day.condition.text,
+          iconPath: f.day.condition.iconPath,
+        ),
+        temperatureRange: TemperatureRangeModelUI(
+          maximum: TemperatureModelUI(
+            celsius: f.day.maxTemp.celsius.round(),
+            fahrenheit: f.day.maxTemp.fahrenheit.round(),
+          ),
+          minimum: TemperatureModelUI(
+            celsius: f.day.minTemp.celsius.round(),
+            fahrenheit: f.day.minTemp.fahrenheit.round(),
+          ),
+        ),
+      );
+    }).toList();
+
+    final todayHourlyForecastUi = todayDomain.hourlyForecast.map((h) {
+      return HourModelUI(
+        dateTime: h.dateTime,
+        conditionIconPath: h.condition.iconPath,
+        temperature: TemperatureModelUI(
+          celsius: h.temperature.celsius.round(),
+          fahrenheit: h.temperature.fahrenheit.round(),
+        ),
+        windSpeed: WindSpeedModelUI(
+          milePerHour: h.windSpeed.milePerHour,
+          kilometrePerHour: h.windSpeed.kilometrePerHour,
+        ),
+      );
+    }).toList();
+
+    final todayAdditionalInfoUI = AdditionalInfoModelUI(
+      wind: WindModelUI(
+        speed: WindSpeedModelUI(
+          milePerHour: current.wind.speed.milePerHour,
+          kilometrePerHour: current.wind.speed.kilometrePerHour,
+        ),
+        direction: current.wind.direction,
+      ),
+      sun: SunModelUI(
+        sunrise: todayDomain.astro.sun.sunrise,
+        sunset: todayDomain.astro.sun.sunset,
+      ),
+      moon: MoonModelUI(
+        phaseImagePath: getMoonPhaseImagePath(todayDomain.astro.moon.moonPhase),
+        phase: todayDomain.astro.moon.moonPhase,
+        moonrise: todayDomain.astro.moon.moonrise,
+        moonset: todayDomain.astro.moon.moonset,
+        illumination: todayDomain.astro.moon.moonIllumination.round(),
+      ),
+      feelsLike: TemperatureModelUI(
+        celsius: current.feelsLike.celsius.round(),
+        fahrenheit: current.feelsLike.fahrenheit.round(),
+      ),
+      humidity: current.humidity,
+      pressure: PressureModelUI(
+        millibars: current.pressure.millibars.round(),
+        inches: current.pressure.inches.round(),
+      ),
+      visibility: VisibilityModelUI(
+        kilometers: current.visibility.kilometers.round(),
+        miles: current.visibility.miles.round(),
+      ),
+      precipitation: PrecipitationModelUI(
+        chanceOfRain: todayDomain.day.precipitation.chanceOfRain,
+        chanceOfSnow: todayDomain.day.precipitation.chanceOfSnow,
+      ),
+    );
+
+    final weeklyForecastDayList = forecast.forecastDayList.take(7).map((f) {
+      return WeeklyForecastDayModelUI(
+        date: f.date,
+        temperature: TemperatureModelUI(
+          celsius: f.day.avgTemp.celsius.round(),
+          fahrenheit: f.day.avgTemp.fahrenheit.round(),
+        ),
+        condition: ConditionModelUI(
+          text: f.day.condition.text,
+          iconPath: f.day.condition.iconPath,
+        ),
+        temperatureRange: TemperatureRangeModelUI(
+          maximum: TemperatureModelUI(
+            celsius: f.day.maxTemp.celsius.round(),
+            fahrenheit: f.day.maxTemp.fahrenheit.round(),
+          ),
+          minimum: TemperatureModelUI(
+            celsius: f.day.minTemp.celsius.round(),
+            fahrenheit: f.day.minTemp.fahrenheit.round(),
+          ),
+        ),
+        uv: f.day.uv.round(),
+        sun: SunModelUI(
+          sunrise: f.astro.sun.sunrise,
+          sunset: f.astro.sun.sunset,
+        ),
+        precipitation: PrecipitationModelUI(
+          chanceOfRain: f.day.precipitation.chanceOfRain,
+          chanceOfSnow: f.day.precipitation.chanceOfSnow,
+        ),
+        humidity: f.day.avgHumidity,
+        visibility: VisibilityModelUI(
+          kilometers: f.day.avgVisability.kilometers.round(),
+          miles: f.day.avgVisability.miles.round(),
+        ),
+        windSpeed: WindSpeedModelUI(
+          milePerHour: f.day.maxWind.milePerHour,
+          kilometrePerHour: f.day.maxWind.kilometrePerHour,
+        ),
+      );
+    }).toList();
+
+    final weeklyMoonList = forecast.forecastDayList.map((f) {
+      return MoonModelUI(
+        phaseImagePath: getMoonPhaseImagePath(f.astro.moon.moonPhase),
+        phase: f.astro.moon.moonPhase,
+        moonrise: f.astro.moon.moonrise,
+        moonset: f.astro.moon.moonset,
+        illumination: f.astro.moon.moonIllumination.round(),
+      );
+    }).toList();
+
+    return WeatherModelUI(
+      location: LocationModelUI(name: location.name, country: location.country),
+      today: TodayModelUI(
+        mainWeather: todayMainWeatherUI,
+        weeklyForecastPreview: weeklyForecastPreviewUI,
+        hourlyForecast: todayHourlyForecastUi,
+        additionalInfo: todayAdditionalInfoUI,
+      ),
+      weeklyForecast: WeeklyForecastModelUI(
+        forecastDayList: weeklyForecastDayList,
+      ),
+      weeklyMoon: WeeklyMoonModelUI(moonList: weeklyMoonList),
+    );
+  }
+}
